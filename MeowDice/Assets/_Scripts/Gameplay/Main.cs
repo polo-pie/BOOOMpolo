9dce4dd041bf39f91cc9d7f21b77ec0aca978246
leo6033 <long452a@163.com>
2023-10-09 22:24:45 +0800

    修复打包文件视频不播放的bug

--- MeowDice/Assets/_Scripts/Gameplay/Main.cs
+++ MeowDice/Assets/_Scripts/Gameplay/Main.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using Engine.UI;
 using MeowDice.GamePlay;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Main : MonoBehaviour
 {
     void Start()
     {
+        var cameraData = Camera.main.GetUniversalAdditionalCameraData();
+        cameraData.cameraStack.Add(UIModule.Instance.UICamera);
         UIModule.Instance.ShowUI<MainWindow>(new Dictionary<string, object>());
     }
 }
